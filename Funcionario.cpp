9a9080c131933ee9c92d9049e329cf0fd4b7b8a8
TEwecher <149436386+TEwecher@users.noreply.github.com>
2024-09-19 01:00:14 +0000

    Cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -8,4 +8,12 @@ class Funcionario: public Pessoa{
         Funcionario(string nome, string sobrenome, long cpf, int dia, int mes, int ano, Endereco endereco, double salario, int cadastro):
         Pessoa(nome, sobrenome, cpf, dia, mes, ano, endereco),
         salario(salario), cadastro(cadastro){}
+
+        virtual void print(){
+            cout << "Dados do funcionario" << endl;
+            cout << "Nome: " << this->getNome() << " " << this->getSobrenome() << endl;
+            cout << "CPF: " << this->getCPF() << endl;
+            cout << "Data de nascimento: " << this->getNascimento() << endl;
+            cout << "Endereco: " << this->getEndereco() << endl;
+        }
 };
